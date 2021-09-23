a6b790cdaf7aeae38f6c7533a5a0c6a1cc5f1ee5
jorawarsingh9380 <jorawar@tatrasdata.com>
2021-09-23 20:12:27 +0530

    add arched

---
+++ apis/auth.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, Response,request,session
+import json
+
+auth_app= Blueprint('auth', __name__)
+
+@auth_app.route('/login',methods=['GET'])
+def login():
+    try:
+        response_data = {"statusCode":200,"body":"Login Successfull"}
+        response = Response(json.dumps(response_data), mimetype='application/json', status=response_data["statusCode"])
+        return response
+    except Exception as e:
+        print("error in login", e)
+        response_data = {'status': 500, 'message': 'Internal Server Error ' + str(e)}
+        response = Response(json.dumps(response_data), mimetype='application/json', status=response_data["status"])
+        return response
+
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from flask import Flask, request,Response
 import json
 from modal.db.connection import connection
+from apis.auth import  auth_app
+
 app = Flask(__name__)
+
+app.register_blueprint(auth_app)
+
 
 
 @app.route('/')
-def hello_world():
-    cursor = connection.engine.execute("select name from d_book")
-    data=cursor.fetchall()
-    list_name =[]
-    for i in data:
-       list_name.append(i[0])
+def hello():
+    response_data = {"statusCode": 200, "body":"hello from find_my_book" }
+    return Response(json.dumps(response_data),mimetype='application/json',status=response_data["statusCode"])
 
-    response_data = {"status": 200, "message": list_name}
-    return Response(json.dumps(response_data),mimetype='application/json',status=response_data["status"])
+#
+# @app.route('/')
+# def hello():
+#     cursor = connection.engine.execute("select name from d_book")
+#     data=cursor.fetchall()
+#     list_name =[]
+#     for i in data:
+#        list_name.append(i[0])
+#
+#     response_data = {"status": 200, "message": list_name}
+#     return Response(json.dumps(response_data),mimetype='application/json',status=response_data["status"])
 
 
 
